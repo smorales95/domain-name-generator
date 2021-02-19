eb6172a737d21f4f14266d25280d75637db64b14
smorales95 <steven_pibe95@hotmail.com>
2021-02-19 02:34:19 +0000

    Ejercio Resuelto

---
+++ src/app.js
@@ -0,0 +1,24 @@
+//console.log("Any string");
+
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let dom =['.net','.us','.org','com'];
+
+
+
+for(let i=0;i<pronoun.length;i++){
+   domi=pronoun[i];
+      for(let j=0;j<adj.length;j++ ){
+          domi=pronoun[i]+adj[j];
+          for(let t=0;t<adj.length;t++){
+              domi=pronoun[i]+adj[j]+noun[t]
+              //console.log(domi +".com");
+              for(let p=0;p<dom.length;p++){
+              domi=pronoun[i]+adj[j]+noun[t]+dom[p]
+              console.log(domi);
+          }
+          }
+      }
+
+}
